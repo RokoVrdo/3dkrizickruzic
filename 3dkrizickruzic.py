452f03cdf78a9cf4ea1537fc9cb38f4a975d1e11
RokoVrdo <106666710+RokoVrdo@users.noreply.github.com>
2022-10-31 12:48:45 +0100

    Add files via upload

---
+++ 3dkrizickruzic.py
@@ -0,0 +1,19 @@
+class TicTacToe:
+    def ploca():
+        ploca1= [None*4
+                None*4
+                None*4
+                None*4] 
+        ploca2= [None*4
+                None*4
+                None*4
+                None*4]
+ploca3= [None*4
+        None*4
+        None*4
+        None*4]
+ploca4= [None*4
+        None*4
+        None*4
+        None*4]
+
