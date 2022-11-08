2e220e807bc742d00ba579c4ea8bcadeb0820ade
RokoVrdo <106666710+RokoVrdo@users.noreply.github.com>
2022-11-08 11:46:33 +0100

    Add files via upload

--- 3dkrizickruzic.py
+++ 3dkrizickruzic.py
@@ -1,7 +1,7 @@
 plocaVelika= [[[None]*4, [None]*4, [None]*4, [None]*4], [[None]*4, [None]*4, [None]*4, [None]*4], [[None]*4, [None]*4, [None]*4, [None]*4], [[None]*4, [None]*4, [None]*4, [None]*4]]
 
 pobjednik=None
-izjednaceno= None
+izjednaceno= False
 trenutni_igrac=None
 broj_krugova=0
 player1=None
@@ -119,6 +119,9 @@ def pobjeda():
                         if pobjednik==None:
                             izjednaceno=True
 
+#funkcija pobjeda provjerava sve moguće slučajeve pobjede, ukupno ih je 76 (10 na svakoj ploči, to je ukupno 40, 
+#prostorne dijagonale kojih je 4, dijagonale između ploča kojih je 8 u jednom smjeru i 8 u drugom i još stupci između ploča kojih je 16)
+
 
 
 def promjena_igraca():
@@ -129,24 +132,42 @@ def promjena_igraca():
     else:
         trenutni_igrac=player2
 
+#funkciji promjena igrača je cilj nakon svakog odigranog kruga (kada jedan igrač napravi potez) promjeniti igrača koji je na redu
+
 #def potez():
     #pesicev dio, ako je odabreno polje različito od None print da je polje zauzeto
+
+#potez je u biti pesicev dio u kojem se na polje stavljaju križić i kuzić, moj je jedini 
+#zadatak u tom dijelu bilo dodati da ako se klikne na polje koje je već zauzeto da javi to igraču i da mu da opet bira polje
     
 
-def scoreboard():
-    global player1, player2, player1_pobjede, player2_pobjede, lista_pobjeda
-    if player1!=lista_pobjeda[0][0]:
-        player1_pobjede=0
-    if player2!=lista_pobjeda[1][0]:
-        player2_pobjede=0
+
+
+
+
+#ova funkcija scoreboard je napravljena u slučaju da će se moći mijenjati ime igrača nakon svake partije, pa da ako ime ostane 
+#isto nastavi brojat pobjede, a ako se ime promijeni da počne od 0, no nakraju funkcija nije trebala u samom kodu jer smo se zbog 
+#jednostavnosti dogovorili da će biti samo player 1 i player 2
+
+#def scoreboard():
+#    global player1_pobjede, player2_pobjede, lista_pobjeda
+#    if player1!=lista_pobjeda[0][0]:
+#        player1_pobjede=0
+#    if player2!=lista_pobjeda[1][0]:
+#        player2_pobjede=0
     
-    lista_pobjeda={player1:player1_pobjede, player2:player2_pobjede}
+#    lista_pobjeda={player1:player1_pobjede, player2:player2_pobjede}
+
+
 
 
 def ako_izjednaceno():
-    global izjednaceno
     if izjednaceno==True:
         print("Izjednaceno je!")
+
+#funkcija ako izjednaceno provjerava je li izjednačen rezultat, tu sam sad stavio print 
+#jer nisam imo ništa drugo, ali u samom programu je drugačije pošto je sučelje grafičko
+
 
 def vracanje_lobby():
     global izjednaceno, pobjednik, broj_krugova, trenutni_igrac, player1, player2
@@ -156,27 +177,35 @@ def vracanje_lobby():
         trenutni_igrac=None
         player1=None
         player2=None
+        izjednaceno=False
+
+#ovoj funkciji je zadatak zapravo bio nakon završetka partije vratiti igru u lobby, odnosno na main menu, 
+#tu još ne dostaje dio u kojem vraća u main menu, budući da je to marta radila
+
+
+
+
+
+#moj dio zadatka je bio, kao što smo Vam već i rekli logika same igrice, tako da sam ja smišljao način kako to sve zajedno povezati
+#ovih nekoliko funkcija je nužno za funkcioniranje same igrice, naravno uz martin i lovrin dio, kada su njih dvoje dostavili svoje dijeove
+#okvirna ideja mi je izgledala ovako:
+
+#while True:
+#martin dio
+#if play:
+#while True:
+#iscrtavanje ploče za igru (pešićev dio)
+#promjena igrača
+#potez
+#pobjeda
+#ako izjednačeno
+#povratak u lobby
+
+
+#tako je otprilike i izgledao kod, možda malo drugačije s obzirom na neke preinake koje smo morali napravit, 
+#ali je princip sa petljom u petlji ostao isti
 
         
 
 
-#display
-#pesicev dio ako je resume
-#elif scoreboard: scoreboard
-#kad se pesicev dio unese: 1)promjena igraca 2)potez 3)pobjeda 4)ako_izjednaceno 5)vracanje u lobby
 
-
-
-
-
-
-#while True:
- #   martin dio
-  #  peskov dio 
-   # 1)prmojena promjena_igraca
-    #2)potez
-    #3)pobjeda
-    #4)izjednaceno
-    #5)vracanje u lobby
-    #scoreborad 
-    #exit-->izlazak iz funckije (igre)
